a79378f822c15fca5324b61fefb2abc7e24647f1
jiali0411 <jyu28@vols.utk.edu>
2019-09-02 17:04:17 -0400

    add solution

---
+++ codes/lgis.py
@@ -0,0 +1,58 @@
+"""
+Created on Sep 1st, 2019
+Author: Jiali
+This is the solution for rosalind question 'Longest Increasing Subsequence'
+"""
+# Give an integer n, followed by a permutation of length n. 
+
+def permutation(seq, reverse):
+    results = []
+# If seq is empty, the permutation is an empty list
+    if len(seq) == 0:
+        return []
+# If there is only one element in lst then, only one permuatation is possible 
+    if len(seq) == 1:
+        results.append(seq)
+        return results
+
+    remain = seq[:-1]
+    remain_list = permutation(remain, reverse)
+
+    for item in remain_list:
+        new_item = item[:]
+        if not reverse:
+            if new_item[-1] < seq[-1]:
+                new_item.append(seq[-1])
+                results.append(new_item)
+            results.append(item)
+        else:
+            if new_item[-1] > seq[-1]:
+                new_item.append(seq[-1])
+                results.append(new_item)
+            results.append(item)
+
+    results.append([seq[-1]])
+    return results
+
+def Longest(results):
+    max_seq = max(results,key=len)
+    max_sub = ""
+    for i in max_seq:
+        max_sub = max_sub+str(i)+" "
+    return max_sub
+
+def main():
+    input_seq = "5 1 4 2 3"
+    seq = input_seq.split(" ")
+    res = permutation(seq, reverse = False)
+    max_inc = Longest(res)
+    new_res = permutation(seq, reverse = True)
+    max_dec = Longest(new_res)
+    print max_inc
+    print max_dec
+main()
+
+"""
+ Reference: the permutaion function is followed 
+ with method 3 in https://www.geeksforgeeks.org/generate-all-the-permutation-of-a-list-in-python/
+"""
